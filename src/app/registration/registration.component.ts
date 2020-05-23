ec3d3841297dde7ee43f1e4ff1f51ecc583e0c13
RAJNISH <10815566+byionline@users.noreply.github.com>
2020-05-23 17:27:14 +0530

    Registration with Form Validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -9,8 +10,31 @@ export class RegistrationComponent implements OnInit {
   Roles: any = ['Admin', 'Author', 'Reader'];
 
   constructor() { }
+  form: FormGroup = new FormGroup({
+    $key: new FormControl(null),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
 
-  ngOnInit(): void {
+    email: new FormControl('', [Validators.required, Validators.email]),
+
+    role: new FormControl('', Validators.required),
+
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  initializeFormGroup() {
+    this.form.setValue({
+      $key: null,
+      username: '',
+      email: '',
+      role: '',
+      password: '',
+    });
   }
-
+  ngOnInit() {}
 }
